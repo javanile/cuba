7acc88c7e654c88db753c6a2b1aa1dd962e1532f
Francesco Bianco <info.francescobianco@gmail.com>
2020-05-12 15:26:22 +0200

    publish

--- installer.sh
+++ installer.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
-set -e
-
 ##
-# cuba
+# CUBA
 # ====
-# by Francesco Bianco
-# bianco@javanile.org
-# MIT License
+#
+# The best LCOV framework around BASH projects.
+#
+# Copyright (c) 2020 Francesco Bianco <bianco@javanile.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 ##
+
+set -e
 
 ##
 # Run as root user.
@@ -31,14 +50,14 @@ main() {
   local cuba_src=${cuba_cdn}/cuba.sh
 
   curl -s ${cuba_cdn}/cuba.txt
-  echo -e "\e[43m\e[97m\e[1m INFO \e[0m Installing 'neo' as root..."
-  run_as_root curl -sLo "${neo_bin}" "${neo_src}?ts=$(date +%s)"
-  run_as_root chmod 755 "${neo_bin}"
-  run_as_root chown 0:0 "${neo_bin}"
-  echo -e "\e[42m\e[97m\e[1m DONE \e[0m Type 'neo --help' to begin."
+  echo -e "\e[43m\e[97m\e[1m INFO \e[0m Installing 'cuba' as root..."
+  run_as_root curl -sLo "${cuba_bin}" "${cuba_src}?ts=$(date +%s)"
+  run_as_root chmod 755 "${cuba_bin}"
+  run_as_root chown 0:0 "${cuba_bin}"
+  echo -e "\e[42m\e[97m\e[1m DONE \e[0m Type 'cuba' to begin."
 }
 
 ##
-#
+# Entry point
 ##
 main "$@"
